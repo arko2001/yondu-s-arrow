ab39a77c5edc089c013cf0493ee34c0082ee94ac
arko2001 <84087208+arko2001@users.noreply.github.com>
2022-06-30 22:06:49 +0530

    Add files via upload
    here the two codes python code which takes whistle signals and convert in frequency domain by applying fft algorithm and the desired number generated is fed to an arduino to move a servo to move the joystick of the drone

---
+++ servofuckinsuccess.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+Servo myServo;
+//int jen;
+int servoPin=9;
+int servoPos=0;
+String msg ="Enter the perfect angle";
+void setup(){
+  //put your setup code here to run once:
+  Serial.begin(9600);
+  myServo.attach(servoPin);
+ 
+}
+void loop() {
+  Serial.println(msg);
+  while(Serial.available()== 0) {
+  }
+  servoPos = Serial.parseInt();
+  if (servoPos >150 && servoPos < 200)
+{
+    Serial.println("Arko foreward");
+    Serial.println(servoPos);
+    myServo.write(180);
+    delay (200);
+    myServo.write(90);
+    }
+    else if (servoPos <100 && servoPos > 50){
+     Serial.println("Arko backward");
+     Serial.println(servoPos);
+    myServo.write(0);
+    delay(200);
+    myServo.write(90);
+
+   
+    }
+    else{
+    Serial.println("oh boy");
+    Serial.println(servoPos);
+    }
+  }
